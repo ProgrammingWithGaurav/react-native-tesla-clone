a6eab215075f38de0ac9505c162487ac6f144b53
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-01-08 21:28:40 +0530

    Finished 🚀

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,40 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     carContainer: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'orange',
+        width: "100%",
+        height: "100%",
+        backgroundColor: "orange"
     },
     header: {
         marginTop: 50,
         marginLeft: 20,
         marginRight: 20,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
         marginBottom: 10
     },
     headerTitle: {
-        color: 'white',
+        color: "white",
         fontSize: 16,
-        fontWeight: 'bold',
-        alignItems: 'center',
-        justifyContent: 'center',
+        fontWeight: "bold",
+        alignItems: "center",
+        justifyContent: "center"
     },
     icon: {
-        color: 'white',
+        color: "white"
     },
     backgroundImage: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
-        position: 'absolute',
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover",
+        position: "absolute"
     },
     batterySection: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
     },
     batteryImage: {
         height: 26,
@@ -42,18 +42,31 @@ const styles = StyleSheet.create({
         marginRight: 12
     },
     batteryText: {
-        color: 'white',
+        color: "white",
         fontSize: 35,
-        fontWeight: 'bold',        
+        fontWeight: "bold"
     },
     status: {
-        alignItems: 'center',
+        alignItems: "center"
     },
     statusText: {
         fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold',
+        color: "white",
+        fontWeight: "bold"
+    },
+    controls: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 175
+    },
+    controlsButton: {
+        borderWidth: 1,
+        borderColor: "white",
+        borderRadius: 50,
+        padding: 18,
+        marginLeft: 25
     }
+
 })
 
 export default styles;
